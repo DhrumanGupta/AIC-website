f291149ab3ec3efa207fb85c28806c2d51829813
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-04-09 13:09:53 +0530

    init, added home page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { cn } from "@/lib/cn";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Logo from "@/../public/logo.png";
+import Image from "next/image";
+
+type TNavLink = {
+  href: string;
+  label: string;
+};
+
+const LINKS: TNavLink[] = [
+  { href: "/members", label: "Members" },
+  { href: "/pitches", label: "Pitches" },
+  { href: "/resources", label: "Resources" },
+];
+
+const NavLink = ({
+  href,
+  label,
+  scrolled,
+}: TNavLink & { scrolled: boolean }) => (
+  <Link
+    href={href}
+    className={cn(
+      "my-auto font-semibold md:text-lg",
+      scrolled ? "text-black" : "text-white"
+    )}
+  >
+    {label}
+  </Link>
+);
+
+function Navbar() {
+  // const { mode, toggle } = useMode();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (!pathname || pathname !== "/") {
+      return;
+    }
+
+    const handleScroll = () => {
+      const scrolled = window.scrollY > window.innerHeight - 25; // 100vh
+      setIsScrolled(scrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [pathname]);
+
+  return (
+    <nav className="flex z-20 top-0 p-6 w-full sticky backdrop-blur-sm md:px-20 lg:px-32">
+      <Link href="/" className="block w-10 h-10 md:w-12 md:h-12 relative">
+        <Image
+          src={Logo}
+          alt="Bodhi Capital Logo"
+          fill={true}
+          className="object-contain"
+        />
+      </Link>
+
+      <div className="flex ml-auto gap-x-4 md:gap-x-16">
+        {LINKS.map((link) => (
+          <NavLink scrolled={isScrolled} key={link.href} {...link} />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+import HeroImage from "@/../public/hero.png";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+function Hero() {
+  return (
+    <div className="h-screen relative w-full">
+      <div className="absolute w-full h-full bg-black"></div>
+      <Image
+        src={HeroImage}
+        layout="fill"
+        alt="Ashoka University"
+        className="object-cover opacity-35 z-10 blur-sm'"
+      />
+      <div className="z-20 text-white w-full h-full px-8 absolute flex flex-col items-center md:items-start justify-center md:pl-32">
+        <h1 className="font-extrabold text-6xl lg:text-8xl md:max-w-2xl uppercase text-center md:text-left md:!leading-[1.1]">
+          <span className="text-primary">Bodhi</span> Capital
+        </h1>
+
+        <h3 className="text-center mt-8 text-xl md:text-2xl md:text-left lg:text-3xl lg:max-w-2xl font-medium">
+          INDIA&apos;S FIRST UNDERGRADUATE INVESTMENT FUND
+        </h3>
+      </div>
+
+      <a
+        className="w-12 h-12 text-center text-white z-30 absolute left-1/2 -translate-x-1/2 bottom-0 mb-16"
+        href="#start"
+      >
+        <IoMdArrowDropdown className="w-full h-full" />
+      </a>
+    </div>
+  );
+}
+
+export default Hero;
