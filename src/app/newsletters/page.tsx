1fa0f36419419c77cf736b7c8055bc420c2a28be
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-09-03 22:15:10 +0530

    newsletters :)

--- src/app/newsletters/page.tsx
+++ src/app/newsletters/page.tsx
@@ -7,10 +7,10 @@ const NewslettersPage = async () => {
 
   return (
     <div className="mt-8">
-      <h1 className="text-4xl font-bold text-center mt-8 mb-4">
+      {/* <h1 className="text-4xl font-bold text-center mt-8 mb-4">
         All Newsletters
-      </h1>
-      <div className="flex flex-col mx-4 sm:mx-12 md:max-w-[85ch] md:mx-auto md:px-4 gap-4">
+      </h1> */}
+      <div className="flex flex-col mx-0 md:max-w-[60ch] md:mx-auto md:px-4 gap-4">
         {newslettersData.map((newsletter: Newsletter) => (
           <div key={newsletter.slug}>
             <NewsletterPreview newsletter={newsletter} />
--- src/components/NewsletterPreview.tsx
+++ src/components/NewsletterPreview.tsx
@@ -1,7 +1,5 @@
-import { cn } from "@/lib/cn";
 import Image from "next/image";
 import Link from "next/link";
-import { CiClock2 } from "react-icons/ci";
 
 type Newsletter = {
   slug: string;
@@ -21,15 +19,36 @@ const NewsletterPreview = ({
   // Image on the left. Title, content, and date on the right. Clicking anywhere on the card should take the user to the newsletter page.
   return (
     <Link href={`/newsletters/${newsletter.slug}`} passHref>
-      <div
+      <div className="grid grid-cols-4 md:grid-cols-10 gap-2 md:gap-4">
+        <div className="col-span-3 md:col-span-7">
+          <h3 className="font-semibold md:text-lg">{newsletter.title}</h3>
+          <p className="text-sm md:mt-1 text-gray-800">
+            Personal finance, investment philosophies and fun facts - all
+            without the jargon.
+          </p>
+
+          <p className="text-xs md:text-sm mt-1 md:mt-2 text-gray-600">
+            {newsletter.date}
+          </p>
+        </div>
+        <div className="relative w-full h-2/3 md:h-[80%] my-auto md:col-span-3">
+          <Image
+            src={`/newsletters/${newsletter.slug}/${newsletter.image}`}
+            alt={newsletter.title}
+            fill={true}
+            className="object-cover"
+          />
+        </div>
+      </div>
+      {/* <div
         className={cn(
-          "flex flex-col md:flex-row w-full cursor-pointer items-stretch border border-gray-300 rounded-md overflow-hidden",
+          "flex flex-col md:flex-row w-full cursor-pointer items-stretch  rounded-md overflow-hidden",
           flip && "md:!flex-row-reverse"
         )}
       >
         <div className="relative w-full md:w-[55%] lg:w-1/2">
           <Image
-            src={newsletter.image}
+            src={`/newsletters/${newsletter.slug}/${newsletter.image}`}
             alt={newsletter.title}
             layout="responsive"
             objectFit="cover"
@@ -48,7 +67,7 @@ const NewsletterPreview = ({
             <p className="text-gray-600">{newsletter.date}</p>
           </div>
         </div>
-      </div>
+      </div> */}
     </Link>
   );
 };
