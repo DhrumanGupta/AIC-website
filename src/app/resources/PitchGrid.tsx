a65b2ba0c0b7b30b8ecef78c889f476a3129d601
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2024-09-18 21:04:04 +0530

    update

--- src/app/resources/PitchGrid.tsx
+++ src/app/resources/PitchGrid.tsx
@@ -3,12 +3,7 @@
 import PDFViewer from "@/components/PDFViewer";
 import { File } from "@/lib/r2";
 import React, { useState } from "react";
-import {
-  FaExternalLinkAlt,
-  FaFileAlt,
-  FaFileExcel,
-  FaFilePdf,
-} from "react-icons/fa";
+import { FaFileAlt, FaFileExcel, FaFilePdf } from "react-icons/fa";
 
 interface PitchGridProps {
   files: File[];
@@ -16,9 +11,9 @@ interface PitchGridProps {
 
 const getFileIcon = (url: string) => {
   if (url.endsWith(".pdf"))
-    return <FaFilePdf className="text-red-500 w-5 h-5 mr-2" />;
+    return <FaFilePdf className="text-red-500 w-12 h-12 mr-2" />;
   if (url.endsWith(".xls") || url.endsWith(".xlsx") || url.endsWith(".csv"))
-    return <FaFileExcel className="text-green-500 w-5 h-5 mr-2" />;
+    return <FaFileExcel className="text-green-500 w-12 h-12 mr-2" />;
   return <FaFileAlt className="text-gray-500 w-5 h-5 mr-2" />;
 };
 
@@ -27,11 +22,11 @@ const PitchGrid: React.FC<PitchGridProps> = ({ files }) => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6">
         {files.map((file) => (
           <div
             key={file.name}
-            className="bg-white p-4 rounded-lg shadow hover:bg-gray-100 transition-colors relative flex items-center justify-between hover:cursor-pointer"
+            className="aspect-square md:aspect-video bg-white p-4 rounded-lg shadow hover:bg-gray-100 transition-colors relative flex flex-col items-center justify-center hover:cursor-pointer"
             onClick={() => {
               if (window.innerWidth > 768) {
                 setSelectedFile(file);
@@ -40,17 +35,16 @@ const PitchGrid: React.FC<PitchGridProps> = ({ files }) => {
               }
             }}
           >
-            <div className="flex items-center flex-grow">
-              {getFileIcon(file.url)}
-              <h3 className="text-sm font-medium text-gray-800 truncate">
-                {file.name
-                  .replace(".pdf", "")
-                  .replace(".xls", "")
-                  .replace(".xlsx", "")
-                  .replace(".csv", "")}
-              </h3>
-            </div>
-            <a
+            {getFileIcon(file.url)}
+            <h3 className="text-sm font-medium text-gray-800 truncate break-words text-wrap text-center mt-5">
+              {file.name
+                .replace(".pdf", "")
+                .replace(".xls", "")
+                .replace(".xlsx", "")
+                .replace(".csv", "")}
+            </h3>
+
+            {/* <a
               href={file.url}
               target="_blank"
               rel="noopener noreferrer"
@@ -59,7 +53,7 @@ const PitchGrid: React.FC<PitchGridProps> = ({ files }) => {
               aria-label={`Open ${file.name} in new tab`}
             >
               <FaExternalLinkAlt />
-            </a>
+            </a> */}
           </div>
         ))}
       </div>
@@ -67,7 +61,7 @@ const PitchGrid: React.FC<PitchGridProps> = ({ files }) => {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg w-11/12 h-5/6 max-w-4xl flex flex-col">
             <div className="flex justify-between items-center p-4 border-b">
-              <h2 className="text-xl font-bold truncate">
+              <h2 className="text-xl font-bold truncate text-wrap">
                 {selectedFile.name}
               </h2>
               <button
